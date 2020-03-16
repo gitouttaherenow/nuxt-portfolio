73a42e6afcc71edb92f41cf24ddc83d8166c4cea
gitouttaherenow <thoshi@gmail.com>
2020-03-17 01:59:30 +0530

    Added work page

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,25 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        My Portfolio Site
-      </h1>
-      <h2 class="subtitle">
-        Thoshi&#39;s Portfolio Site
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<span>
+	<HomeHero />
+	<HomeAbout />
+	<HomeProjects />
+	<Contact />
+</span>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import HomeHero from '@/components/HomeHero';
+import HomeAbout from '@/components/HomeAbout';
+import HomeProjects from '@/components/HomeProjects';
+import Contact from '@/components/Contact';
 
 export default {
-  components: {
-    Logo
-  }
+	name: 'home',
+	components: {
+		HomeHero,
+		HomeAbout,
+		HomeProjects,
+		Contact
+	}
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ pages/work/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <HomeProjects></HomeProjects>
+    </div>
+</template>
+
+<script>
+import HomeProjects from '@/components/HomeProjects';
+
+export default {
+    components: {
+		HomeProjects
+	}
+};
+</script>
